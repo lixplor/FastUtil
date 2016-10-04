028c196e40ba8b76a3c7e1de2f882050589d62a9
fantasymaker <me@fantasymaker.cn>
2016-10-04 11:22:06 +0800

    revise RadixUtil, add unit test

--- anutil/src/main/java/cn/fantasymaker/anutil/utils/convert/RadixUtil.java
+++ anutil/src/main/java/cn/fantasymaker/anutil/utils/convert/RadixUtil.java
@@ -37,6 +37,13 @@ public class RadixUtil {
     字符串转十六进制
      */
 
+    private static final char[] HEX_ARR_UPPER = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+    };
+    private static final char[] HEX_ARR_LOWER = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
+
     private RadixUtil() throws IllegalAccessException {
         throw new IllegalAccessException("Instantiation is not allowed! Use static methods only!");
     }
@@ -63,57 +70,56 @@ public class RadixUtil {
 
     public static StringBuffer toHex(byte b) {
         StringBuffer stringBuffer = new StringBuffer();
-        char[] hexChars = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
-                '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+//        char[] hexChars = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
+//                '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int high = ((b & 0xf0) >> 4);
         int low = (b & 0x0f);
-        stringBuffer.append(hexChars[high]);
-        stringBuffer.append(hexChars[low]);
+        stringBuffer.append(HEX_ARR_UPPER[high]);
+        stringBuffer.append(HEX_ARR_UPPER[low]);
         return stringBuffer;
     }
 
 
-    public static String byteArrayToHexString(byte[] bytes){
+    public static String byteArrayToHexString(byte[] bytes) {
         int length = bytes.length;
         StringBuffer stringBuffer = new StringBuffer();
-        for(int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             stringBuffer.append(String.format("%2x", bytes[i]));
         }
         return new String(bytes);
     }
 
-    private static char[] hexArray = "0123456789ABCDEF".toCharArray();
     public static String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
+        for (int j = 0; j < bytes.length; j++) {
             int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+            hexChars[j * 2] = HEX_ARR_UPPER[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARR_UPPER[v & 0x0F];
         }
         return new String(hexChars);
     }
 
-    public static String byteToBinaryString(byte b){
+    public static String byteToBinaryString(byte b) {
         return "todo";
     }
 
-    public static String byteToBinaryString(String byteString){
+    public static String byteToBinaryString(String byteString) {
         return Byte.toString(Byte.parseByte(byteString, 2));
     }
 
-    public static String intToString(String intString, int radix){
+    public static String intToString(String intString, int radix) {
         return Integer.toString(Integer.parseInt(intString), radix);
     }
 
-    public static String intToBinaryString(String intString){
+    public static String intToBinaryString(String intString) {
         return Integer.toBinaryString(Integer.parseInt(intString));
     }
 
-    public static String intToOctalString(String intString){
+    public static String intToOctalString(String intString) {
         return Integer.toOctalString(Integer.parseInt(intString));
     }
 
-    public static String intToHexString(String intString){
+    public static String intToHexString(String intString) {
         return Integer.toHexString(Integer.parseInt(intString));
     }
 
